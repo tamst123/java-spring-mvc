29af16f3911b5b1d561997070dafd3f1922a02b3
tamst123 <sitamtran779@gmail.com>
2025-04-07 13:12:53 +0700

    setup enviroment

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,22 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World 8081 !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello User !";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Hello Admin !";
+    }
+}
